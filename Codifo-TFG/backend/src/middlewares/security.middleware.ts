9b8223e6cfefce8832f6f7294077210bb5a88608
Carlos Arana <carlosaranajimenez@gmail.com>
2025-06-11 13:51:01 +0200

    arreglo galeria 2

--- Codifo-TFG/backend/src/middlewares/security.middleware.ts
+++ Codifo-TFG/backend/src/middlewares/security.middleware.ts
@@ -55,6 +55,7 @@ export const protectApi = (req: Request, res: Response, next: NextFunction): voi
 // Rutas que deben ser públicas (login, registro, etc.)
 const publicRoutes = [
   '/webhook/stripe',
+  '/uploads',
   '/api/user/login',
   '/api/user/logout',
   '/api/user/confirm',
